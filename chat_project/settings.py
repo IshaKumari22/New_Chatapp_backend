0a2007bfee0fc61e34d15d9300aa35fa9d372b11
Amankumar10 <amankumar02740@gmail.com>
2025-10-19 22:36:33 +0530

    intigrate tinylamma model locallly

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'channels',
     'chat',
-    'corsheaders',
+    'api',
 ]
 
 REST_FRAMEWORK={
